b7d89a375a108362ce0e6a14846204c095ef7e12
star <32487675+yyx248131667@users.noreply.github.com>
2024-05-15 20:24:28 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -102,9 +102,5 @@ sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/lu
 sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
 sed -i 's|admin\\|admin\\/services\\/|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
 
-# 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
-cat >>"$DELETE" <<-EOF
-EOF
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
